5b1c2f87a29fd79f0fea5ce682242a6b9a14582a
muzammil <muzammil.modal786@gmail.com>
2026-05-19 10:00:38 +0530

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FincludTech',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title:
-      'Flutter Demo Home Page'),
+      'FincludTech Home Page'),
     );
   }
 }
